fd962d1452ff5e70b17ba658d3abc6c89d2bc0b6
DavidCouronne <couronne.david@gmail.com>
2018-11-30 17:50:03 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -46,7 +46,7 @@ export default {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
-  font-size: 80px;
+  font-size: 70px;
   color: #35495e;
   letter-spacing: 1px;
 }
