c4aa815d1bc84f87933108c9fb20390ba7986b06
DavidCouronne <couronne.david@gmail.com>
2019-01-06 19:04:21 +0100

    Update index.js

--- mathssyfy/.vuepress/param-katex/index.js
+++ mathssyfy/.vuepress/param-katex/index.js
@@ -165,7 +165,7 @@ module.exports = function math_plugin(md, options) {
         options.displayMode = false;
         options.throwOnError = false;
         options.macros = macroskatex;
-        latex = newcommand.toString + latex;
+        latex = newcommand + latex;
         
         try{
             return katex.renderToString(latex, options);
