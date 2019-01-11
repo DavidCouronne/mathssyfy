371a30a646f1e1a0fee25a6142e514eb6714ce33
DavidCouronne <couronne.david@gmail.com>
2019-01-11 13:28:18 +0100

    clean

--- packages/markdown/__tests__/markdown.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const markdown = require('..');
-
-describe('markdown', () => {
-    it('needs tests');
-});
--- packages/markdown/lib/markdown.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = markdown;
-
-function markdown() {
-    // TODO
-}
