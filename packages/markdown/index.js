23c422b705b9f7e44f54dd5259ca46a274ea6745
DavidCouronne <couronne.david@gmail.com>
2019-01-16 20:28:26 +0100

    Add @mathssyfy/markdown-it-loader

--- packages/markdown/index.js
+++ packages/markdown/index.js
@@ -1,169 +1,68 @@
-'use strict'
+// https://github.com/BlueOakJS/markdownit-loader
+// https://github.com/markdown-it/markdown-it
+const path = require('path')
 
-/**
- * Module dependencies.
- */
+module.exports = function nuxtMarkdownit (options) {
+  const _options = Object.assign({}, options, this.options.markdownit)
 
-const Config = require('markdown-it-chain')
-const highlight = require('./lib/highlight')
-const { PLUGINS, REQUIRED_PLUGINS } = require('./lib/constant')
-const highlightLinesPlugin = require('./lib/highlightLines')
-const preWrapperPlugin = require('./lib/preWrapper')
-const lineNumbersPlugin = require('./lib/lineNumbers')
-const componentPlugin = require('./lib/component')
-const hoistScriptStylePlugin = require('./lib/hoist')
-const convertRouterLinkPlugin = require('./lib/link')
-const containersPlugin = require('./lib/containers')
-const markdownSlotsContainersPlugin = require('./lib/markdownSlotsContainers')
-// const snippetPlugin = require('./lib/snippet')
-const emojiPlugin = require('markdown-it-emoji')
-const anchorPlugin = require('markdown-it-anchor')
-const tocPlugin = require('markdown-it-table-of-contents')
-const { parseHeaders, slugify: _slugify } = require('@mathssyfy/shared-utils')
-
-/**
- * Create markdown by config.
- */
-
-module.exports = (markdown = {}) => {
-  const {
-    externalLinks,
-    anchor,
-    toc,
-    lineNumbers,
-    beforeInstantiate,
-    afterInstantiate
-  } = markdown
-
-  // allow user config slugify
-  const slugify = markdown.slugify || _slugify
-
-  // using chainedAPI
-  const config = new Config()
-
-  config
-    .options
-      .html(true)
-      .highlight(highlight)
-      .end()
-
-    .plugin(PLUGINS.COMPONENT)
-      .use(componentPlugin)
-      .end()
-
-    .plugin(PLUGINS.HIGHLIGHT_LINES)
-      .use(highlightLinesPlugin)
-      .end()
-
-    .plugin(PLUGINS.PRE_WRAPPER)
-      .use(preWrapperPlugin)
-      .end()
-
-    .plugin(PLUGINS.SNIPPET)
-      .use(snippetPlugin)
-      .end()
-
-    .plugin(PLUGINS.CONVERT_ROUTER_LINK)
-      .use(convertRouterLinkPlugin, [Object.assign({
-        target: '_blank',
-        rel: 'noopener noreferrer'
-      }, externalLinks)])
-      .end()
-
-    .plugin(PLUGINS.HOIST_SCRIPT_STYLE)
-      .use(hoistScriptStylePlugin)
-      .end()
-
-    .plugin(PLUGINS.CONTAINERS)
-      .use(containersPlugin)
-      .end()
-
-    .plugin(PLUGINS.MARKDOWN_SLOTS_CONTAINERS)
-      .use(markdownSlotsContainersPlugin)
-      .end()
-
-    .plugin(PLUGINS.EMOJI)
-      .use(emojiPlugin)
-      .end()
-
-    .plugin(PLUGINS.ANCHOR)
-      .use(anchorPlugin, [Object.assign({
-        slugify,
-        permalink: true,
-        permalinkBefore: true,
-        permalinkSymbol: '#'
-      }, anchor)])
-      .end()
-
-    .plugin(PLUGINS.TOC)
-      .use(tocPlugin, [Object.assign({
-        slugify,
-        includeLevel: [2, 3],
-        format: parseHeaders
-      }, toc)])
-      .end()
-
-  if (lineNumbers) {
-    config
-      .plugin(PLUGINS.LINE_NUMBERS)
-        .use(lineNumbersPlugin)
+  const markDownItLoader = {
+    loader: '@mathssyfy/markdownit-loader',
+    options: _options
   }
 
-  beforeInstantiate && beforeInstantiate(config)
+  const version = this.nuxt.constructor.version
+  const [major, minor, patch] = version.split('.')
 
-  const md = config.toMd(require('markdown-it'), markdown)
-
-  afterInstantiate && afterInstantiate(md)
-
-  module.exports.dataReturnable(md)
-
-  // expose slugify
-  md.slugify = slugify
-
-  return md
-}
-
-module.exports.dataReturnable = function dataReturnable (md) {
-  // override render to allow custom plugins return data
-  const render = md.render
-  md.render = (...args) => {
-    md.$data = {}
-    md.$data.__data_block = {}
-    md.$dataBlock = md.$data.__data_block
-    const html = render.call(md, ...args)
-    return {
-      html,
-      data: md.$data,
-      dataBlockString: toDataBlockString(md.$dataBlock)
-    }
-  }
-}
-
-function toDataBlockString (ob) {
-  if (Object.keys(ob).length === 0) {
-    return ''
-  }
-  return `<data>${JSON.stringify(ob)}</data>`
-}
-
-function isRequiredPlugin (plugin) {
-  return REQUIRED_PLUGINS.includes(plugin)
-}
-
-function removePlugin (config, plugin) {
-  console.log(`Built-in markdown-it plugin was removed.`)
-  config.plugins.delete(plugin)
-}
-
-function removeAllBuiltInPlugins (config) {
-  Object.keys(PLUGINS).forEach(key => {
-    if (!isRequiredPlugin(PLUGINS[key])) {
-      removePlugin(config, PLUGINS[key])
+  this.extendBuild(config => {
+    if (major === '1') {
+      // Vue template support
+      const vueLoader = config.module.rules.find(rule => rule.test.toString() === '/\\.vue$/')
+      // Checks for query loaders first (<= rc5)
+      if (vueLoader.query && vueLoader.query.loaders) {
+        vueLoader.query.loaders['md'] = markDownItLoader
+      } else {
+        // Sets options loaders (>= rc6)
+        if (!vueLoader.options.loaders) {
+          vueLoader.options.loaders = {}
+        }
+        vueLoader.options.loaders['md'] = markDownItLoader
+      }
+      // .md Loader
+      config.module.rules.push({
+        test: /\.md$/,
+        use: [
+          'raw-loader',
+          markDownItLoader
+        ]
+      })
+    } else {
+      config.module.rules.push({
+        test: /\.md$/,
+        oneOf: [
+          {
+            resourceQuery: /^\?vue/,
+            use: [markDownItLoader]
+          },
+          {
+            use: [
+              'raw-loader',
+              markDownItLoader
+            ]
+          }
+        ]
+      })
     }
   })
+
+  if (_options.injected === true) {
+    delete _options.injected
+    // Register plugin
+    this.addPlugin({
+      src: path.resolve(__dirname, 'plugin.js'),
+      fileName: 'markdown-it.js',
+      options: _options
+    })
+  }
 }
 
-module.exports.isRequiredPlugin = isRequiredPlugin
-module.exports.removePlugin = removePlugin
-module.exports.removeAllBuiltInPlugins = removeAllBuiltInPlugins
-module.exports.PLUGINS = PLUGINS
+module.exports.meta = require('./package.json')
