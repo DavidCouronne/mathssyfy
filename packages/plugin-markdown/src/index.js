4e98776262d770da3a82b701595c1759ebea4ae6
DavidCouronne <couronne.david@gmail.com>
2019-01-18 22:46:21 +0100

    Update index.js

--- packages/plugin-markdown/src/index.js
+++ packages/plugin-markdown/src/index.js
@@ -34,29 +34,7 @@ import tocPlugin from 'markdown-it-table-of-contents'
 
 export default {
 
-  md: new markdownIt({preset: 'default',
-  html: true,
-  highlight: function (str, lang) {
-    lang = lang.toLowerCase()
-  const rawLang = lang
-  if (lang === 'vue' || lang === 'html') {
-    lang = 'markup'
-  }
-  if (lang === 'md') {
-    lang = 'markdown'
-  }
-  if (lang === 'ts') {
-    lang = 'typescript'
-  }
-  if (lang === 'py') {
-    lang = 'python'
-  }
-  if (prism.languages[lang]) {
-    const code = prism.highlight(str, prism.languages[lang], lang)
-    return `<pre v-pre class="language-${rawLang}"><code>${code}</code></pre>`
-  }
-    return `<pre v-pre class="language-${lang}"><code>${str}</code></pre>`;
-  }}),
+  md: new markdownIt(),
 
 
 
