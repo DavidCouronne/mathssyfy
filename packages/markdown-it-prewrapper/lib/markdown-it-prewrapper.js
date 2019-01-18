6c72269e9f94b748cb44a5f41d5e2dc64626b588
DavidCouronne <couronne.david@gmail.com>
2019-01-18 23:23:25 +0100

    Update markdown-it-prewrapper.js

--- packages/markdown-it-prewrapper/lib/markdown-it-prewrapper.js
+++ packages/markdown-it-prewrapper/lib/markdown-it-prewrapper.js
@@ -6,15 +6,16 @@
 //   2. <!--afterbegin-->
 //   3. <!--beforeend-->
 //   4. <!--afterend-->
-
+const CODE = /(<code class="[^>]*>)/g;
 module.exports = md => {
     const fence = md.renderer.rules.fence
     md.renderer.rules.fence = (...args) => {
       const [tokens, idx] = args
       const token = tokens[idx]
       const rawCode = fence(...args)
+      const modifCode = rawCode.replace(CODE,'<code>')
       return `<!--beforebegin--><div class="language-${token.info.trim()} extra-class">` +
-      `<!--afterbegin-->${rawCode}<!--beforeend--></div><!--afterend-->`
+      `<!--afterbegin-->${modifCode}<!--beforeend--></div><!--afterend-->`
     }
   }
   
